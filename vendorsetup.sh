da37354515cedb27ff064a3083746f3389ebb154
tomyprs <tomy.prs25@gmail.com>
2022-11-09 19:43:00 +0700

    prebuilt: chnage with pocom5s
    Signed-off-by: tomyprs <tomy.prs25@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,7 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 		# Device info
 		export OF_AB_DEVICE=1
 		export OF_VIRTUAL_AB_DEVICE=1
-		export TARGET_DEVICE_ALT="secret, maltose"
+		export TARGET_DEVICE_ALT="secret, maltose, rosemaryp"
 		
 		# OTA / DM-Verity / Encryption
 		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
